a8735e11e0e26e770c53623bedb892da957c6319
Ryazanov Alexander Mihailovich <alryaz@alryaz.com>
2024-05-21 23:41:57 +0300

    refactor: force listen on all ipv4 interfaces

--- telegraf/run.sh
+++ telegraf/run.sh
@@ -111,7 +111,7 @@ else
     bashio::log.info "Updating config for Syslog"
     {
       echo "[[inputs.syslog]]"
-      echo "  server = 'udp4://:6514'"
+      echo "  server = 'udp4://0.0.0.0:6514'"
     } >> $CONFIG
   fi
 
